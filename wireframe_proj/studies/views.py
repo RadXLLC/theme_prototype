8e12018194cca489e62038dd1f7b12161c0b56c2
Carmelo Interone <interoneca@comcast.net>
2015-09-15 01:00:13 -0500

    Upload Study
    views.py - add fetch_study_info
    studies_index.html - use iframe to upload.html on archive instead of
    coloradmin upload.

--- wireframe_proj/studies/views.py
+++ wireframe_proj/studies/views.py
@@ -1,7 +1,14 @@
+import json
+import re
+import urllib2
+
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Context
 from django.template.loader import get_template
+from django.shortcuts import render_to_response
+from django.template.context_processors import csrf
 
 # Create your views here.
 def studies_index(request):
@@ -13,5 +20,12 @@ def studies_index(request):
 			'page_body': 'These are the studies',
 		})
 	output = template.render(variables)
-
 	return HttpResponse(output)
+
+def fetch_study_info(request):
+	requestURL = 'http://52.11.158.42:8080/dcm4chee-arc/qido/DCM4CHEE/studies'
+	requestURL += '?includefield=all&StudyInstanceUID=' + data
+
+	req = urllib2.Request((requestURL),headers = {'Accept': 'application/json'})
+	res = urllib2.urlopen(req)
+	return HttpResponse(res.read(), content_type="application/json")
